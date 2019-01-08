a1e9a36b79363b43f373c4585b6dbb33bb530734
Abhinav Singh <adityasaha890@gmail.com>
2019-01-08 16:55:48 +0530

    t

--- route.js
+++ route.js
@@ -4,7 +4,7 @@ const passport=require('passport');
 
 router.get('/login',passport.authenticate('facebook',{scope:['public_profile']}));
 
-router.get('/success',passport.authenticate('facebook',{failureRedirect:'/',successRedirect:'http://localhost:3002/home.html'}));
+router.get('/success',passport.authenticate('facebook',{failureRedirect:'/',successRedirect:'https://evening-falls-93839.herokuapp.com/home.html'}));
 
 module.exports={
     router
