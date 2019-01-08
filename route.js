8105988caf3fc6dab25b722b0bc1bab7dc8920ef
Abhinav Singh <adityasaha890@gmail.com>
2019-01-08 17:51:43 +0530

    Dg

--- route.js
+++ route.js
@@ -2,7 +2,7 @@ const router=require('express').Router();
 const passport=require('passport');
 
 
-router.get('/login',passport.authenticate('facebook',{scope:['public_profile','user_friends']}));
+router.get('/login',passport.authenticate('facebook',{scope:['public_profile','user_friends','user_gender']}));
 
 router.get('/success',passport.authenticate('facebook',{failureRedirect:'/',successRedirect:'https://evening-falls-93839.herokuapp.com/home.html'}));
 
